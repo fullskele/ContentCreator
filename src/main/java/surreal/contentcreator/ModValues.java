544806b087cc3692675393bc6b3616e6abb3d370
fullskele <jacobmajosi@gmail.com>
2024-11-13 15:21:01 -0500

    v1.3.9

--- src/main/java/surreal/contentcreator/ModValues.java
+++ src/main/java/surreal/contentcreator/ModValues.java
@@ -3,5 +3,5 @@ package surreal.contentcreator;
 public class ModValues {
     public static final String MODID = "contentcreator";
     public static final String NAME = "Content Creator";
-    public static final String VERSION = "1.3.6-2";
+    public static final String VERSION = "1.3.6-3";
 }

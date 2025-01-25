160a87efa93528561b3f77939b53481ceb16b668
fullskele <8630389+fullskele@users.noreply.github.com>
2025-01-24 19:26:00 -0500

    Fix for #6
    Double slabs should now retain the properties of their half-slabs counterparts

--- src/main/java/surreal/contentcreator/common/block/generic/BlockGenericDoubleSlab.java
+++ src/main/java/surreal/contentcreator/common/block/generic/BlockGenericDoubleSlab.java
@@ -7,15 +7,19 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 import surreal.contentcreator.ModValues;
 import surreal.contentcreator.util.GeneralUtil;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Random;
 
 public class BlockGenericDoubleSlab extends BlockGenericSlab {
     private final Block halfSlab;
@@ -36,9 +40,10 @@ public class BlockGenericDoubleSlab extends BlockGenericSlab {
         return super.setSoundType(soundType);
     }
 
+    @Nonnull
     @Override
-    public Item createItem(Block block) {
-        return null;
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return Item.getItemFromBlock(halfSlab);
     }
 
     @SuppressWarnings("deprecation")
@@ -46,6 +51,18 @@ public class BlockGenericDoubleSlab extends BlockGenericSlab {
     @Override
     public ItemStack getItem(@Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
         return new ItemStack(halfSlab);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public float getBlockHardness(IBlockState blockState, World worldIn, BlockPos pos) {
+        return halfSlab.getBlockHardness(blockState, worldIn, pos);
+    }
+
+    @Override
+    public float getExplosionResistance(World world, BlockPos pos, @Nullable Entity exploder, Explosion explosion) {
+
+        return halfSlab.getExplosionResistance(world, pos, exploder, explosion);
     }
 
     @Override
